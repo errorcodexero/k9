cf27b817602ef4d24fb5ea7f863804e1a8712a0f
Steve Tarr <stevet@RHUBARB>
2014-01-29 00:03:15 -0800

    added injector

--- k9.cpp
+++ k9.cpp
@@ -4,10 +4,14 @@
 
 const double minSpeed = 1000.;
 const double maxSpeed = 3500.;
+const double initialTopSpeed = 1200.;
+const double initialBottomSpeed = 3200.;
 
 class ShootyDogThing : public IterativeRobot
 {
+    Compressor *compressor;
     CANJaguar *topWheel, *bottomWheel;
+    DoubleSolenoid *injector;
     Joystick *gamepad;
     double kP, kI, kD;
     double topSpeed, bottomSpeed;
@@ -15,14 +19,16 @@ class ShootyDogThing : public IterativeRobot
 
 public:
     ShootyDogThing():
+	compressor(NULL),
 	topWheel(NULL),
 	bottomWheel(NULL),
+	injector(NULL),
 	gamepad(NULL),
 	kP(1.000),
 	kI(0.005),
 	kD(0.000),
-	topSpeed(0.000),
-	bottomSpeed(0.000),
+	topSpeed(0.),
+	bottomSpeed(0.),
 	report(0)
     {
 	this->SetPeriod(0); 	//Set update period to sync with robot control packets (20ms nominal)
@@ -31,8 +37,10 @@ public:
     ~ShootyDogThing()
     {
 	delete gamepad;
+	delete injector;
 	delete bottomWheel;
 	delete topWheel;
+	delete compressor;
     }
     
     /**
@@ -42,28 +50,36 @@ public:
      * be called when the robot is first powered on.  It will be called exactly 1 time.
      */
     void ShootyDogThing::RobotInit() {
-	topWheel = new CANJaguar(5);
+
+	compressor  = new Compressor(1, 1);
+
+	topWheel    = new CANJaguar(5);
+	topWheel->SetSafetyEnabled(false);	// motor safety off while configuring
+
 	bottomWheel = new CANJaguar(6);
-	gamepad = new Joystick(1);
+	bottomWheel->SetSafetyEnabled(false);	// motor safety off while configuring
+
+	injector    = new DoubleSolenoid(1, 2);
+	
+	gamepad     = new Joystick(1);
 
 	LiveWindow *lw = LiveWindow::GetInstance();
-	lw->AddActuator("K9", "Top", topWheel);
-	lw->AddActuator("K9", "Bottom", bottomWheel);
-
-	topWheel->SetSafetyEnabled(false);	// motor safety off while configuring
-	bottomWheel->SetSafetyEnabled(false);	// motor safety off while configuring
+	lw->AddActuator("K9", "Compressor", compressor);
+	lw->AddActuator("K9", "Top",        topWheel);
+	lw->AddActuator("K9", "Bottom",     bottomWheel);
+	lw->AddActuator("K9", "Injector",   injector);
 
 	SmartDashboard::PutNumber("Shooter P", kP);
 	SmartDashboard::PutNumber("Shooter I", kI);
 	SmartDashboard::PutNumber("Shooter D", kD);
 
-	SmartDashboard::PutNumber("Top Speed", topSpeed);
-	SmartDashboard::PutNumber("Top Voltage", 0.0);
-	SmartDashboard::PutNumber("Top RPM", 0.0);
+	SmartDashboard::PutNumber("Top Speed",    topSpeed);
+	SmartDashboard::PutNumber("Top Voltage",  0.0);
+	SmartDashboard::PutNumber("Top Measured", 0.0);
 
-	SmartDashboard::PutNumber("Bottom Speed", bottomSpeed);
-	SmartDashboard::PutNumber("Bottom Voltage", 0.0);
-	SmartDashboard::PutNumber("Bottom RPM", 0.0);
+	SmartDashboard::PutNumber("Bottom Speed",    bottomSpeed);
+	SmartDashboard::PutNumber("Bottom Voltage",  0.0);
+	SmartDashboard::PutNumber("Bottom Measured", 0.0);
     }
 
     /**
@@ -73,8 +89,10 @@ public:
      * the robot enters disabled mode. 
      */
     void ShootyDogThing::DisabledInit() {
+
 	topSpeed = 0.0;
 	SmartDashboard::PutNumber("Top Speed", topSpeed);
+
 	bottomSpeed = 0.0;
 	SmartDashboard::PutNumber("Bottom Speed", bottomSpeed);
 
@@ -86,6 +104,8 @@ public:
 	topWheel->SetSafetyEnabled(false);
 	bottomWheel->DisableControl();
 	bottomWheel->SetSafetyEnabled(false);
+
+	compressor->Stop();
     }
 
     /**
@@ -122,6 +142,10 @@ public:
      * the robot enters teleop mode.
      */
     void ShootyDogThing::TeleopInit() {
+
+	compressor->Start();
+	injector->Set(DoubleSolenoid::kReverse);
+
 	// Set control mode
 	topWheel->ChangeControlMode( CANJaguar::kSpeed );
 	bottomWheel->ChangeControlMode( CANJaguar::kSpeed );
@@ -144,9 +168,9 @@ public:
 	// Enable Jaguar control:
 	// Increase motor safety timer to something suitably long
 	// Poke the motor speed to reset the watchdog, then enable the watchdog
-	topSpeed = 1200.0;
+	topSpeed = initialTopSpeed;
 	SmartDashboard::PutNumber("Top Speed", topSpeed);
-	bottomSpeed = 3200.0;
+	bottomSpeed = initialBottomSpeed;
 	SmartDashboard::PutNumber("Bottom Speed", bottomSpeed);
 
 	topWheel->EnableControl();
@@ -173,32 +197,35 @@ public:
      * rate while the robot is in teleop mode.
      */
     void ShootyDogThing::TeleopPeriodic() {
-	if (gamepad->GetRawButton(4)) {		// "Y"
+	if (gamepad->GetRawButton(1)) {
 	    topSpeed = minSpeed + (maxSpeed - minSpeed) * (1.0 - gamepad->GetRawAxis(2)) / 2.0;
 	    SmartDashboard::PutNumber("Top Speed", topSpeed);
 	} else {
 	    topSpeed = SmartDashboard::GetNumber("Top Speed");
 	}
-	if (gamepad->GetRawButton(1)) {		// "A"
+
+	if (gamepad->GetRawButton(3)) {
 	    bottomSpeed = minSpeed + (maxSpeed - minSpeed) * (1.0 - gamepad->GetRawAxis(2)) / 2.0;
 	    SmartDashboard::PutNumber("Bottom Speed", bottomSpeed);
 	} else {
 	    bottomSpeed = SmartDashboard::GetNumber("Bottom Speed");
 	}
+
 	topWheel->Set(topSpeed, SYNC_GROUP);
 	bottomWheel->Set(bottomSpeed, SYNC_GROUP);
 	CANJaguar::UpdateSyncGroup(SYNC_GROUP);
+
 	if (++report >= 25) {	// 500 milliseconds
 	    double topV = topWheel->GetOutputVoltage();
-	    double topRPM = topWheel->GetSpeed(); 
+	    double topMeasured = topWheel->GetSpeed(); 
 	    double bottomV = bottomWheel->GetOutputVoltage();
-	    double bottomRPM = bottomWheel->GetSpeed(); 
+	    double bottomMeasured = bottomWheel->GetSpeed(); 
 
 	    // Send values to SmartDashboard
 	    SmartDashboard::PutNumber("Top Voltage", topV);
-	    SmartDashboard::PutNumber("Top RPM", topRPM);
+	    SmartDashboard::PutNumber("Top Measured", topMeasured);
 	    SmartDashboard::PutNumber("Bottom Voltage", bottomV);
-	    SmartDashboard::PutNumber("Bottom RPM", bottomRPM);
+	    SmartDashboard::PutNumber("Bottom Measured", bottomMeasured);
 
 	    // Update PID parameters
 	    kP = SmartDashboard::GetNumber("Shooter P");
@@ -209,6 +236,20 @@ public:
 
 	    report = 0;
 	}
+
+	if (gamepad->GetRawButton(4))
+	{
+	    injector->Set(DoubleSolenoid::kForward);
+	}
+	else if (gamepad->GetRawButton(2))
+	{
+	    injector->Set(DoubleSolenoid::kReverse);
+	}
+	else
+	{
+	    injector->Set(DoubleSolenoid::kOff);
+	}
+
     }
 
     /**
@@ -218,6 +259,10 @@ public:
      * the robot enters test mode.
      */
     void ShootyDogThing::TestInit() {
+
+	compressor->Start();
+	injector->Set(DoubleSolenoid::kOff);
+
 	topWheel->ChangeControlMode( CANJaguar::kPercentVbus );
 	bottomWheel->ChangeControlMode( CANJaguar::kPercentVbus );
 
